95ead24a884abb33c9ba18f5aeed002d100efd7f
Bashir <bashierb96@gmail.com>
2023-07-13 10:18:07 +0200

    added btn action and list into js

--- JS/code.js
+++ JS/code.js
@@ -4,7 +4,6 @@ const TodoInput = document.querySelector('.todo-input');
 const TodoBtn = document.querySelector('.todo-btn');
 const TodoResult = document.querySelector('.todo-result');
 
-
 function addTodo(e) {
     e.preventDefault();
     console.log(TodoInput.value);
@@ -12,12 +11,38 @@ function addTodo(e) {
     const todoLi = document.createElement('li');
     todoLi.classList.add('my-2');
 
+
     const todoTitle = document.createElement('h3');
     todoTitle.innerText = TodoInput.value;
+    TodoInput.value = '';
     todoTitle.classList.add('d-inline-block', 'me-5');
+
+
+    const TodoAction = document.createElement('div');
+    TodoAction.classList.add('d-inline-block', 'float-end');
+
+
+    const btnComplete = document.createElement('button');
+    btnComplete.classList.add('btn', 'btn-outline-primary');
+    btnComplete.innerHTML = ' <i class="fa-sharp fa-solid fa-check"></i>';
+    TodoAction.appendChild(btnComplete);
+
+    const btnDelete = document.createElement('button')
+    btnDelete.classList.add('btn','btn-outline-warning')
+    btnDelete.innerHTML='<i class="fa-regular fa-trash-can"></i>'
+    TodoAction.appendChild(btnDelete)
+
+
+    const btnEdit = document.createElement('button')
+    btnEdit.classList.add('btn','btn-outline-info')
+    btnEdit.innerHTML='<i class="fa-solid fa-keyboard"></i>'
+    TodoAction.appendChild(btnEdit)
+
+
 
     todoLi.appendChild(todoTitle);
     TodoResult.appendChild(todoLi);
+    todoLi.appendChild(TodoAction);
 }
 
 TodoBtn.addEventListener('click', addTodo);
